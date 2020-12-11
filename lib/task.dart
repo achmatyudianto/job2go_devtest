9ee443d07734ced5b0098c3c8925340d4371e065
yudi <achmatyudianto@gmail.com>
2020-12-11 13:17:06 +0700

    Added update&create task

--- lib/task.dart
+++ lib/task.dart
@@ -17,6 +17,7 @@ class TaskPage extends StatefulWidget {
 class _TaskPageState extends State<TaskPage> {
   final listtask = List<TaskModel>();
   bool loading = false;
+  TextEditingController contentDesc = TextEditingController();
 
   Future<void> _readData() async {
     setState(() {
@@ -32,7 +33,7 @@ class _TaskPageState extends State<TaskPage> {
         final ab = TaskModel(
           api['id'],
           api['content'],
-          api['due']['date'],
+          //api['due']['date'],
         );
         listtask.add(ab);
       });
@@ -41,6 +42,139 @@ class _TaskPageState extends State<TaskPage> {
       loading = false;
     });
     print(listtask.length);
+  }
+
+  Future<void> _updateData(BuildContext context, id) async {
+    var data = {
+      'content': contentDesc.text,
+    };
+    var response = await CallApi().postData(data, 'tasks/$id');
+    Navigator.pop(context);
+    _readData();
+  }
+
+  Future<void> _saveData(BuildContext context) async {
+    var data = {
+      'content': contentDesc.text,
+      'project_id': widget.projectModel.id
+    };
+    var response = await CallApi().postData(data, 'tasks');
+    Navigator.pop(context);
+    _readData();
+  }
+
+  dialogUpdate(BuildContext context, TaskModel x) {
+    contentDesc.text = x.content;
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return ListView(
+                padding: EdgeInsets.all(16.0),
+                shrinkWrap: true,
+                children: <Widget>[
+                  TextField(
+                    controller: contentDesc,
+                    autofocus: false,
+                    decoration: new InputDecoration(
+                      contentPadding: EdgeInsets.all(13.0),
+                      labelText: "Content",
+                      labelStyle: TextStyle(fontSize: 13.0),
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(15.0),
+                        borderSide: new BorderSide(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  RaisedButton(
+                    onPressed: () {
+                      _updateData(context, x.id);
+                    },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                    child: Container(
+                      height: 40.0,
+                      child: Center(
+                        child: Text(
+                          'Update',
+                          style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 0.5,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  dialogCreate(BuildContext context) {
+    contentDesc.text = "";
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return ListView(
+                padding: EdgeInsets.all(16.0),
+                shrinkWrap: true,
+                children: <Widget>[
+                  TextField(
+                    controller: contentDesc,
+                    autofocus: false,
+                    decoration: new InputDecoration(
+                      contentPadding: EdgeInsets.all(13.0),
+                      labelText: "Content",
+                      labelStyle: TextStyle(fontSize: 13.0),
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(15.0),
+                        borderSide: new BorderSide(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  RaisedButton(
+                    onPressed: () {
+                      _saveData(context);
+                    },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                    child: Container(
+                      height: 45.0,
+                      child: Center(
+                        child: Text(
+                          'Save',
+                          style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 0.5,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -66,19 +200,29 @@ class _TaskPageState extends State<TaskPage> {
                 final x = listtask[i];
                 return ListTile(
                   title: Text(x.content),
-                  subtitle: Text("due ${x.due}"),
+                  //subtitle: Text("due ${x.due}"),
                   trailing: Container(child: Icon(Icons.edit)),
                   onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => TaskPage(x),
-                    //   ),
-                    // );
+                    dialogUpdate(context, x);
                   },
                 );
               },
             ),
+      floatingActionButton: Container(
+        width: 53.0,
+        height: 53.0,
+        child: FloatingActionButton(
+          onPressed: () {
+            dialogCreate(context);
+          },
+          backgroundColor: Colors.blue,
+          tooltip: 'Add',
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
